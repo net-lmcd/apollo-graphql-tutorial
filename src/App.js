5c001ad2b05a336fd456d371abe2f9e931c92d6d
Donguk <mbxd1@naver.com>
2019-11-06 00:28:41 +0900

    fetchMore 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import './App.css';
-import { Query } from 'react-apollo'
-import { gql } from 'apollo-boost'
-import MyRepositores from './Myrepositories'
+import React from 'react'
+import './App.css'
+import {Query} from 'react-apollo'
+import {gql} from 'apollo-boost'
+import MyRepositories from './MyRepositories'
 
 const query = gql`
   {
@@ -15,22 +15,24 @@ const query = gql`
 
 function App() {
   return (
-    <div className="App">
-      <Query query={query}>
-        {result => {
-          if (result.loading) return <p>loading...</p>;
-          if (result.error) return <p>{result.error.message}</p>;
-          return(
-              <div>
-                <h1> name : {result.data.viewer.name}</h1>
-                <p>  Email : {result.data.viewer.email}</p>
-              </div>
-          )
-        }}
-      </Query>
-      <MyRepositores/>
-    </div>
-  );
+      <>
+        <div className="App">
+          <Query query={query}>
+            {result => {
+              if (result.loading) return <p>loading...</p>
+              if (result.error) return <p>{result.error.message}</p>
+              return (
+                  <div>
+                    <h1> name : {result.data.viewer.name}</h1>
+                    <p> Email : {result.data.viewer.email}</p>
+                  </div>
+              )
+            }}
+          </Query>
+        </div>
+        <MyRepositories/>
+      </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/MyRepositories.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Query } from 'react-apollo'
+import { gql } from 'apollo-boost'
+
+const reposQuery = gql`
+ query MyRepositories($first : Int!){
+   viewer{
+    repositories(first : $first){
+      edges{
+        node {
+          name
+        }
+      }
+    }
+  }
+ }
+`
+
+const MyRepositories = () => {
+  const handleMore = (current, fetchMore, data) => {
+    fetchMore({
+      variables : {first : current + 3},
+      updateQuery : (prev, {fetchMoreResult}) => {
+        if(!fetchMoreResult) return prev
+        else {
+         return Object.assign({}, prev, fetchMoreResult)
+        }
+      }
+    })
+  }
+  return(
+      <Query query={reposQuery} variables={{first : 6}}>
+        {({loading, error, data, fetchMore}) => {
+          if (loading) return <p>loading...</p>
+          if (error) return <p>{error.message}</p>
+          let current = data.viewer.repositories.edges.length
+          return(
+                <div>
+                  <ul>
+                    <h2>First {current} repositories</h2>
+                    {data.viewer.repositories.edges.map(({node},index) => {
+                      return(
+                          <li key={index}>{node.name}</li>
+                      )
+                    })
+                    }
+                  </ul>
+                  <button onClick={()=>handleMore(current, fetchMore, data)}>Fetch More</button>
+                </div>
+            )
+          }
+        }
+      </Query>
+  )
+}
+
+export default MyRepositories
