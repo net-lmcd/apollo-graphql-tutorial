695b5d2865e9b0d1923f74cb0d62f9489954702b
Donguk <mbxd1@naver.com>
2019-11-05 19:03:35 +0900

    깃허브 토큰설정, 깃허브graphql API 요청

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import {render} from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import ApolloClient, {gql} from 'apollo-boost'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  request: operation => {
+    operation.setContext({
+      headers : {
+        authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`
+      }
+    })
+  }
+})
+
+client
+    .query({
+      query: gql`
+        query GetnameandEmail {
+          viewer {
+            email
+            name
+          }
+        }
+      `
+    })
+    .then(res => console.log(res))
+
+
+render(<App/>, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
